283cc58801d0363e8b1fdcd8abb63188644191bb
Daniel Bachhuber <daniel@handbuilt.co>
2017-04-14 14:13:41 -0700

    Initial commit

---
+++ bylines.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Bylines
+ * Plugin URI:      https://bylines.io
+ * Description:     Modern multi-author publishing for WordPress
+ * Author:          Daniel Bachhuber, Hand Built
+ * Author URI:      https://handbuilt.co
+ * Text Domain:     bylines
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Bylines
+ */
