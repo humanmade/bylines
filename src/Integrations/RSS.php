605c822b0fa3e7aa55e9994b2236bb4ae0ea99be
Daniel Bachhuber <daniel@handbuilt.co>
2017-05-03 12:23:06 -0700

    Only render bylines in feed for supported post types

--- src/Integrations/RSS.php
+++ src/Integrations/RSS.php
@@ -7,6 +7,8 @@
 
 namespace Bylines\Integrations;
 
+use Bylines\Content_Model;
+
 /**
  * Render bylines within WordPress' default RSS feed templates.
  */
@@ -18,9 +20,10 @@ class RSS {
 	 * @param string $author Existing author string.
 	 */
 	public static function filter_the_author( $author ) {
-		if ( ! is_feed() ) {
+		if ( ! is_feed() || ! self::is_supported_post_type() ) {
 			return $author;
 		}
+
 		$bylines = get_bylines();
 		$first = array_shift( $bylines );
 		return ! empty( $first ) ? $first->display_name : '';
@@ -30,12 +33,30 @@ class RSS {
 	 * Add any additional bylines to the feed.
 	 */
 	public static function action_rss2_item() {
+		if ( ! self::is_supported_post_type() ) {
+			return;
+		}
 		$bylines = get_bylines();
 		// Ditch the first byline, which was already rendered above.
 		array_shift( $bylines );
 		foreach ( $bylines as $byline ) {
 			echo '<dc:creator><![CDATA[' . esc_html( $byline->display_name ) . ']]></dc:creator>' . PHP_EOL;
 		}
+	}
+
+	/**
+	 * Whether or not the global post is a supported post type
+	 *
+	 * @return boolean
+	 */
+	private static function is_supported_post_type() {
+		global $post;
+
+		// Can't determine post, so assume true.
+		if ( ! $post ) {
+			return true;
+		}
+		return in_array( $post->post_type, Content_Model::get_byline_supported_post_types(), true );
 	}
 
 }
