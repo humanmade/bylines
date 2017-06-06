18ffeda3fd37a89e5a29194a6fd1c58ff97efb7d
Daniel Bachhuber <daniel@handbuilt.co>
2017-06-06 15:47:42 -0700

    Add a basic Behat test for the command, which fails

---
+++ src/CLI.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Manage bylines.
+ *
+ * @package Bylines
+ */
+
+namespace Bylines;
+
+/**
+ * * Manage bylines.
+ */
+class CLI {
+
+	/**
+	 * Generate byline terms for one or more posts.
+	 *
+	 * ## OPTIONS
+	 *
+	 * <post-id>...
+	 * : One or more post ids to generate bylines for.
+	 *
+	 * @subcommand generate-for-posts
+	 */
+	public function generate_for_posts( $args, $assoc_args ) {
+		// @todo implement command.
+	}
+
+}
